3921d150aea582c6acfcd7de585a9e11849ef922
Lunalaaaa <tania.kicien@etu.umontpellier.fr>
2023-04-20 16:39:42 +0200

    mise en place de add, update, delete et reset

--- src/Controller/ToDoController.php
+++ src/Controller/ToDoController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Sodium\add;
 
 class ToDoController extends AbstractController
 {
@@ -27,7 +29,7 @@ class ToDoController extends AbstractController
     }
 
     #[Route('/todo/add/{name}/{content}', name: 'todo.add')]
-    public function addTodo(Request $request, $name, $content){
+    public function addTodo(Request $request, $name, $content): RedirectResponse{
         $session = $request->getSession();
         if($session->has('tabToDo')){
             $tabToDo = $session->get('tabToDo');
@@ -36,13 +38,60 @@ class ToDoController extends AbstractController
             }
             else{
                 $tabToDo[$name] = $content;
-                $this->addFlash('success', "Le todo $name avec le contenu $content a été ajouté");
                 $session->set('tabToDo', $tabToDo);
+                $this->addFlash('success', "Le todo $name avec le contenu $content a été ajouté");
             }
         }
         else{
             $this->addFlash('error', 'La liste des todos n\'est pas encore initialisée.');
         }
+        return $this->redirectToRoute('todo');
+    }
+
+    #[Route('todo/update/{name}/{content}', name: 'todo.update')]
+    public function updateTodo(Request $request, $name, $content): RedirectResponse{
+        $session = $request->getSession();
+        if($session->has('tabToDo')){
+            $tabTodo = $session->get('tabToDo');
+            if(isset($tabTodo[$name])){
+                $tabTodo[$name] = $content;
+                $session->set('tabToDo', $tabTodo);
+                $this->addFlash('success', "L'élément de clé $name a été mis à jour avec $content.");
+            }
+            else{
+                $this->addFlash('error', "La clé $name n'existe pas.");
+            }
+        }
+        else{
+            $this->addFlash('error', 'La liste des todos n\'est pas encore initialisée.');
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    #[Route('todo/delete/{name}', name: 'todo.delete')]
+    public function deleteTodo(Request $request, $name): RedirectResponse{
+        $session = $request->getSession();
+        if($session->has('tabToDo')){
+            $tabTodo = $session->get('tabToDo');
+            if(isset($tabTodo[$name])){
+                unset($tabTodo[$name]);
+                $session->set('tabToDo', $tabTodo);
+                $this->addFlash('success', "L'élément de clé $name a bien été supprimé");
+            }
+            else{
+                $this->addFlash('error', "La clé $name n'existe pas.");
+            }
+        }
+        else{
+            $this->addFlash('error', 'La liste des todos n\'est pas encore initialisée.');
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    #[Route('todo/reset', name: 'todo.reset')]
+    public function resetTodo(Request $request): RedirectResponse{
+        $session = $request->getSession();
+        $session->remove('tabToDo');
         return $this->redirectToRoute('todo');
     }
 }
